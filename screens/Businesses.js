4b344aedac09dd37f348916edc72cf6a880fa155
Jeremiah Hawks <mallardknundrum@gmail.com>
2019-07-13 18:18:43 -0400

    Got tableview showing

--- screens/Businesses.js
+++ screens/Businesses.js
@@ -1,9 +1,42 @@
 import React from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList, Text, StyleSheet, View } from 'react-native';
+import businessData from '../data.json';
+
+
 
 export default class Businesses extends React.Component {
+
+  // constructor (props) {
+  //   super(props);
+  //   this.state = {
+  //     dataSource: [],
+  //   }
+  // }
+  // componentDidMount() {
+  //   console.log(businessData)
+  //   this.setState({dataSource: businessData})  
+  // }
   render() {
-    return <Text>Foo bar shibby</Text>
-    // return <FlatList />
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={businessData}
+          renderItem={({item})=> <Text style={styles.item}>{item.name}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44
+  },
+})
