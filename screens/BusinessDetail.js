09f46af6bcc796caa53cfa8614b1444e8be8337b
Jeremiah Hawks <mallardknundrum@gmail.com>
2019-07-14 20:06:45 -0400

    a little little better

--- screens/BusinessDetail.js
+++ screens/BusinessDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, console, requireNativeComponent, Dimensions } from 'react-native';
+import { View, Text, console, requireNativeComponent, Dimensions, StyleSheet } from 'react-native';
 
 const ChartView = requireNativeComponent("ChartView")
 
@@ -9,20 +9,40 @@ export default class BusinessDetail extends React.Component {
     const company = navigation.getParam("otherParam", "NoCompany");
     
     const jsonString = JSON.stringify(company)
-    // ChartView.dataDictionaries = { 1 }
-    // console.log(ChartView.dataDictionaries)
-    // console.log({JSON.stringify(company)});
 
     return (
     <View style={{flex: 1, justifyContent: 'center'}}>
-      <View style={{flex: 1}}>
-        <Text style={{fontSize: 10}}>{JSON.stringify(company)}</Text>
+      <View style={styles.flexVerticalContainer}>
+        <Text style={styles.title}>{company.name}</Text>
+        <Text style={styles.locationText}>{company.location.city + ", " + company.location.country}</Text>
       </View>
       <ChartView 
-        style={{flex: 3, width: Dimensions.get("window").width}}
+        style={{flex: 7}}
         dataDictionaries={company}
         myNumber={2}
       />
     </View>);
   }
 }
+
+const styles = StyleSheet.create({
+  flexVerticalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    borderRadius: 45,
+    borderWidth: 15,
+    borderStyle: 'solid',
+    borderColor: '#B5F0EA',
+    flexDirection: 'column'
+  },
+  title: {
+    fontSize: 19,
+    fontWeight: 'bold',
+    textAlignVertical: 'bottom',
+    textAlign: 'center',
+  },
+  locationText: {
+    fontSize: 12,
+    textAlign: 'center',
+  },
+})
